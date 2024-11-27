27ee13d7d5cba71e8df80c88632c392c3d51920d
kamila dos santos <107414257+saik-chan@users.noreply.github.com>
2024-11-27 15:49:31 -0300

    quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,60 +1,59 @@
-import { criarGrafico, getCSS, incluirTexto } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
-async function redesSociaisFavoritasMinhaEscola() {
-    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
-    if (dadosLocaisString) {
-        const dadosLocais = JSON.parse(dadosLocaisString)
-        processarDados(dadosLocais)
-    } else {
-        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
-        const res = await fetch(url)
-        const dados = await res.json()
-        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
-        processarDados(dados)
-    }
-}
-
-function processarDados(dados) {
-    const redesSociais = dados.slice(1).map(redes => redes[1])
-    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
-        acc[redesSociais] = (acc[redesSociais] || 0) + 1
-        return acc
-    }, {})
-    const valores = Object.values(contagemRedesSociais)
-    const labels = Object.keys(contagemRedesSociais)
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
-            values: valores,
-            labels: labels,
-            type: 'pie',
-            textinfo: 'label+percent'
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
     ]
 
-    const layout = {
+    const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
-        height: 700,
         title: {
-            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            text: 'Redes sociais com mais usuários',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
-                family: getCSS('--font'),
-                size: 30
+                size: 30,
+                font: getCSS('--font')
             }
         },
-        legend: {
-            font: {
-                color: getCSS('--primary-color'),
-                size: 16
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
             }
         }
     }
 
-    criarGrafico(data, layout)
-    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
 }
 
-redesSociaisFavoritasMinhaEscola()
+quantidadeUsuariosPorRede()
